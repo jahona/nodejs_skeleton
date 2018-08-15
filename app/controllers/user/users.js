03fc7affb5d8a388f0a4f4c0b333c7e2fea07e43
jahona <jahona5240@gmail.com>
2018-08-15 13:49:58 +0900

    crud user api

--- app/controllers/user/users.js
+++ app/controllers/user/users.js
@@ -1,33 +1,88 @@
 "use strict";
 
+const User = require("../../models/user");
+
 module.exports = {
   getUserList: async (req, res, next) => {
-    res.json({
-      success: "getUserList"
-    });
+    const users = await User.find({}).select("-userPw");
+
+    res.status(200).json(users);
   },
 
   createUser: async (req, res, next) => {
-    res.json({
-      success: "createUser"
-    });
+    let user;
+
+    user = await User.findOne({ userId: req.body.userId });
+
+    if (user) {
+      return res.status(409).json({
+        error: "UserId-already-existed",
+        code: 0
+      });
+    }
+
+    const newUser = new User(req.body);
+
+    user = await newUser.save();
+
+    res.status(200).json(user);
   },
 
   getUser: async (req, res, next) => {
-    res.json({
-      success: "getUser"
-    });
+    const { uid } = req.params;
+
+    const user = await User.findById(uid, { userPw: false });
+
+    if (!user) {
+      return res.status(404).json({
+        error: {
+          message: "user-not-found",
+          code: 0
+        }
+      });
+    }
+
+    res.status(200).json(user);
   },
 
   updateUser: async (req, res, next) => {
-    res.json({
-      success: "updateUser"
-    });
+    const { uid } = req.params;
+
+    const user = await User.findByIdAndUpdate(uid, req.body, {
+      new: true
+    }).select("-userPw");
+
+    if (!user) {
+      return res.status(404).json({
+        error: {
+          message: "user-not-found",
+          code: 0
+        }
+      });
+    }
+
+    res.status(200).json(user);
   },
 
   deleteUser: async (req, res, next) => {
-    res.json({
-      success: "deleteUser"
+    const { uid } = req.params;
+
+    const user = await User.findById(uid);
+    if (!user) {
+      return res.status(404).json({
+        error: {
+          message: "user-not-found",
+          code: 0
+        }
+      });
+    }
+
+    await User.remove({ _id: uid });
+
+    res.status(200).json({
+      success: {
+        message: "success-delete-user"
+      }
     });
   }
 };
