ad4bcd8361dfe1ae6ccdcdedbae22b1d3fd879a2
jahona <jahona5240@gmail.com>
2018-08-16 17:34:24 +0900

    socket.io basic chatting

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const env = require("./config/environment");
 const mysql = require("mysql");
+const socket = require("socket.io");
+const http = require("http");
 
 var app = express();
+
+/* express http 서버 생성 */
+const server = http.createServer(app);
+
+// http 서버를 socket.io에 바인딩
+const io = socket(server);
 
 // view
 app.set("views", __dirname + "/views");
@@ -82,9 +90,50 @@ app.use((err, req, res, next) => {
   console.error(err);
 });
 
+// socket.io 이벤트 등록
+io.sockets.on("connection", function(socket) {
+  /* 새로운 유저가 접속했을 경우 다른 소켓에게도 알려줌 */
+  socket.on("newUser", function(name) {
+    console.log(name + " 님이 접속하였습니다.");
+
+    /* 소켓에 이름 저장해두기 */
+    socket.name = name;
+
+    /* 모든 소켓에게 전송 */
+    io.sockets.emit("update", {
+      type: "connect",
+      name: "SERVER",
+      message: name + "님이 접속하였습니다."
+    });
+  });
+
+  /* 전송한 메시지 받기 */
+  socket.on("message", function(data) {
+    /* 받은 데이터에 누가 보냈는지 이름을 추가 */
+    data.name = socket.name;
+
+    console.log(data);
+
+    /* 보낸 사람을 제외한 나머지 유저에게 메시지 전송 */
+    socket.broadcast.emit("update", data);
+  });
+
+  /* 접속 종료 */
+  socket.on("disconnect", function() {
+    console.log(socket.name + "님이 나가셨습니다.");
+
+    /* 나가는 사람을 제외한 나머지 유저에게 메시지 전송 */
+    socket.broadcast.emit("update", {
+      type: "disconnect",
+      name: "SERVER",
+      message: socket.name + "님이 나가셨습니다."
+    });
+  });
+});
+
 //Start the Server
 const port = env.port || 8888;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
 
 module.exports = app;
